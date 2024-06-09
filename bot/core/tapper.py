6832f25a6edbd21591991102827785d4c37b2bde
Ramesh Shanilka <ramesh.shanilka@gmail.com>
2024-06-09 19:25:07 +0530

    bug fix

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -144,7 +144,15 @@ class Tapper:
         except Exception as error:
             logger.error(
                 f"{self.session_name} | Proxy: {proxy} | Error: {error}")
-
+            
+   async def check_proxy(self, http_client: aiohttp.ClientSession, proxy: Proxy) -> None:
+        try:
+            response = await http_client.get(url='https://httpbin.org/ip', timeout=aiohttp.ClientTimeout(5))
+            ip = (await response.json()).get('origin')
+            logger.info(f"{self.session_name} | Proxy IP: {ip}")
+        except Exception as error:
+            logger.error(f"{self.session_name} | Proxy: {proxy} | Error: {error}")
+            
     async def run(self, proxy: str | None) -> None:
         access_token = ''
         user_data = {}
